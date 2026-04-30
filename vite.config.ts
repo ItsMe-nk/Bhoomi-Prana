92b5cef187b18f8b4645d0eb288cadc5923c2078
ItsMe-nk <contactnayankumar@gmail.com>
2026-04-30 21:46:01 +0530

    Add Tailwind CSS plugin to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: '/Bhoomi-Prana/', // This tells Vite your site is in a subfolder
 })
